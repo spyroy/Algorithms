9fe707f87b2f0876bf625f0b898dbf41e37c6667
matan <57196433+spyroy@users.noreply.github.com>
2020-01-30 00:32:12 +0200

    turtle and rabbit

---
+++ TurtleAndRabbit/LinkedListCycle.java
@@ -0,0 +1,59 @@
+package TurtleAndRabbit;
+
+
+/**
+ * this class represents a linked list with a cycle, it has a pointer to the
+ * head which represents the start of the cycle, and tail which is the previous
+ * of the head
+ * 
+ * @author spyro
+ *
+ */
+public class LinkedListCycle {// Double Cycle Linked List
+	private Node head, tail;
+
+	// Constructor, constructs an empty list
+	public LinkedListCycle() {
+		head = tail = null;
+	}
+
+	/**
+	 * this function will add an element to the list, the element will become the
+	 * tail
+	 * 
+	 * @param obj
+	 */
+	// Appends the specified element to the end of this list.
+	public void add(char obj) {
+		if (head == null) {
+			head = new Node(obj, null, null);
+			tail = head;
+			head.next = head.prev = tail;
+		} else {
+			Node n = new Node(obj, tail, head);
+			tail.next = n;
+			tail = n;
+			head.prev = tail;
+		}
+	}
+
+	public Node getHead() {
+		return head;
+	}
+
+	public Node getNext(Node n) {
+		return n.next;
+	}
+
+	public String toString() {
+		String s = "[";
+		if (head != null) {
+			s = s + head.toString() + ", ";
+			for (Node n = head.next; n != head; n = n.next) {
+				s = s + n.toString() + ", ";
+			}
+			s = s.substring(0, s.length() - 2);
+		}
+		return s + "]";
+	}
+}// end LinkedListCycle
---
+++ TurtleAndRabbit/Node.java
@@ -0,0 +1,40 @@
+package TurtleAndRabbit;
+
+/**
+ * this class is represent a node in the LinkedListCycle class, each node has a
+ * pointer to the next node, a pointer to the previous node, head and data
+ * 
+ * @author spyro
+ *
+ */
+
+public class Node {
+	char data;
+	Node prev, next;
+
+	public Node(char data, Node prev, Node next) {
+		this.data = data;
+		this.next = next;
+		this.prev = prev;
+	}
+
+	public String toString() {
+		return "" + this.data;
+	}
+
+	public void setData(char c) {
+		this.data = c;
+	}
+
+	public char getData() {
+		return this.data;
+	}
+
+	public Node getNext() {
+		return this.next;
+	}
+
+	public Node getPrev() {
+		return this.prev;
+	}
+}
